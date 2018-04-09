0484ca863853810448718a7a0c34f236579cbd4b
thepalry <theparly@gmail.com>
2018-04-09 17:01:32 +0900

    Update Sample
    크롤링을 금하는 Naver robots 정책에 따라서 Daum 부동산 정보 크롤링으로 샘플을 변경

--- sample.js
+++ sample.js
@@ -1,35 +1,26 @@
 const crawler = require('./main');
 
-// const url = 'http://land.naver.com/article/complexInfo.nhn?rletTypeCd=A01&tradeTypeCd=&rletNo=2';
-// console.log( $("[title='시/도'] option:selected").text());
-// console.log( $("[title='시/군/구'] option:selected").text());
-// console.log( $("[title='읍/면/동'] option:selected").text());
-// console.log( $("[title='단지명'] option:selected").text());
-
 var rletNoArray = [];
-for(var i=0;i<10;i++) {
+for(var i=1;i<=10;i++) {
     rletNoArray.push(i);
 }
 
 crawler.makeRequest({
-    url : 'http://land.naver.com/article/complexInfo.nhn',
+    url : 'http://realestate.daum.net/iframe/maemul/DanjiInfo.daum',
     staticParams : [
-        { name : 'rletTypeCd', value : 'A01'},
-        { name : 'tradeTypeCd', value : ''}
+        { name : 'tabName', value : 'info'}
     ],
     dynamicParams : [
-        { name : 'rletNo', value : rletNoArray}
+        { name : 'danjiId', value : rletNoArray}
     ],
-    maxConnection : 3,
+    maxConnection : 5,
     timeInterval : 0.5
 });
 
 crawler.responseHandler({
     selectors : [
-        "[title='시/도'] option:selected",
-        "[title='시/군/구'] option:selected",
-        "[title='읍/면/동'] option:selected",
-        "[title='단지명'] option:selected"
+        "#subTabDanji > div.tab_menu.clearfix > h3",
+        "#descAddr"
     ]
 });
 
